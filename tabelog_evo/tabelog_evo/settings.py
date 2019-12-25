b30de2b1db71846d0fd3273fa94ece6fb09ba727
Yuto-Aoki <yuuto3039@gmail.com>
2019-12-25 17:09:09 +0900

    Add DataBase url

--- tabelog_evo/tabelog_evo/settings.py
+++ tabelog_evo/tabelog_evo/settings.py
@@ -70,7 +70,8 @@ DOWNLOAD_DELAY = 5
 ITEM_PIPELINES = {
    'tabelog_evo.pipelines.ValidationPipeline': 300,
 }
-
+# DB
+POSTGRESQL_URL = 'postgresql://tabelog_evo:tabelog_evo_pass@localhost:5432/tabelog_evo_db'
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
