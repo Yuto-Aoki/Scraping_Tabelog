a4d68bf2e679f187ff6befb4b202a0c9bcc47199
Yuto-Aoki <yuuto3039@gmail.com>
2020-02-10 23:46:29 +0900

    Add schedule spider

---
+++ tabelog_evo/schedulespider.py
@@ -0,0 +1,22 @@
+from twisted.internet import reactor, defer
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
+
+settings = get_project_settings()
+
+settings.set('FEED_URI', 'results_%(filename)s.csv')
+
+configure_logging()
+runner = CrawlerRunner(settings)
+
+
+@defer.inlineCallbacks
+def crawl():
+    yield runner.crawl('tabelog', comment='test', filename='tabelog')
+    yield runner.crawl('retty', page_limit=1, comment='test',filename='retty')
+    reactor.stop()
+
+
+crawl()
+reactor.run()
